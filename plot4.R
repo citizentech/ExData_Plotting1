3059ca175e30c112e6fd225558015bc828d46e0d
CEO <onomesheriff@hotmail.com>
2019-05-06 11:22:32 +0100

    HOUSEHOLD ELECTRICITY CONSUMPTION
    MY ASSIGNMENT

---
+++ plot4.R
@@ -0,0 +1,80 @@
+---
+title: "Untitled"
+author: "LOFOR ANDREW"
+date: "May 2, 2019"
+output: ioslides_presentation
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+```
+
+## R Markdown
+
+This is an R Markdown presentation. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+
+When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document.
+
+## Slide with Bullets
+
+- Bullet 1
+- Bullet 2
+- Bullet 3
+
+## Slide with R Output
+
+```{r cars, echo = TRUE}
+summary(cars)
+```
+
+## Slide with Plot
+
+```{r pressure}
+plot(pressure)
+```
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Format date to Type Date
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+  
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+  
+## Remove incomplete observation
+t <- t[complete.cases(t),]
+
+## Combine Date and Time column
+dateTime <- paste(t$Date, t$Time)
+  
+## Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+  
+## Remove Date and Time column
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+  
+## Add DateTime column
+t <- cbind(dateTime, t)
+  
+## Format dateTime Column
+t$dateTime <- as.POSIXct(dateTime)
+## Create plot4
+
+
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(t, {
+  plot(Global_active_power~dateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~dateTime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~dateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~dateTime,col='Red')
+  lines(Sub_metering_3~dateTime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~dateTime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+## Saving to file
+#dev.copy(png, file="plot4.png", height=480, width=480)
+#dev.off()
